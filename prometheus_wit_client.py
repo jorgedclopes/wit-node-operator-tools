743d1d50cc5a163fa7351817a3bef7141732e927
Jorge Lopes <jorgedclopes@gmail.com>
2022-01-26 00:35:14 +0000

    Add all the metrics and start generalising the code.

--- prometheus_wit_client.py
+++ prometheus_wit_client.py
@@ -5,11 +5,59 @@ import docker
 from typing import List
 import re
 
+import singleton
+
 required_container = 'witnet/witnet-rust'
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
+
+
+def append_to_list(class_list, metric_name, container_name):
+    class_list.append(
+        prometheus_client.Gauge(
+            (metric_name + '_' + container_name).replace('-', '_'),
+            metric_name))
+
+
+@singleton
+class WitnetMetrics:
+    proposed_blocks_list = []
+    blocks_in_blockchain_list = []
+    eligible_list = []
+    proposed_commits_list = []
+    accepted_commits_list = []
+    slashed_commits_list = []
+    reputation_list = []
+
+    def __init__(self, container_list):
+        for container in container_list:
+            WitnetMetrics.proposed_blocks_list.append(
+                prometheus_client.Gauge(
+                    ('Proposed_blocks_' + container.name).replace('-', '_'),
+                    'Proposed blocks'))
+            append_to_list(WitnetMetrics.proposed_blocks_list,
+                           'Proposed_blocks',
+                           container.name)
+            append_to_list(WitnetMetrics.blocks_in_blockchain_list,
+                           'Blocks_in_blockchain',
+                           container.name)
+            append_to_list(WitnetMetrics.eligible_list,
+                           'Times_elegible',
+                           container.name)
+            append_to_list(WitnetMetrics.proposed_commits_list,
+                           'Proposed_commits',
+                           container.name)
+            append_to_list(WitnetMetrics.accepted_commits_list,
+                           'Accepted_commits',
+                           container.name)
+            append_to_list(WitnetMetrics.slashed_commits_list,
+                           'Slashed_commits',
+                           container.name)
+            append_to_list(WitnetMetrics.reputation_list,
+                           'Reputation_list',
+                           container.name)
 
 
 # Decorate function with metric.
@@ -24,13 +72,16 @@ def process_request(container_list,
     for c, gauge in zip(container_list, gauge_list):
         _, output = c.exec_run('witnet node nodeStats')
         logger.debug(output)
-        interesting_value = func(output.decode('utf-8'))
+        interesting_value = func(output.decode('utf-8'), 'Proposed blocks: ', '\n')
         gauge.set(interesting_value)
     time.sleep(10)
 
 
-def get_proposed_blocks(string):
-    return int(re.search('Proposed blocks: (.*)\n', string).group(1))
+def search_from_pattern(string, begin, end):
+    try:
+        return int(re.search(begin + '(.*)' + end, string).group(1))
+    except:
+        return -1
 
 
 def check_pattern_in_tags(string: str, str_list: List[str]):
@@ -38,7 +89,6 @@ def check_pattern_in_tags(string: str, str_list: List[str]):
 
 
 if __name__ == '__main__':
-    nodes_gauge_list = []
     # Start up the server to expose the metrics.
     client = docker.from_env()
 
@@ -48,11 +98,7 @@ if __name__ == '__main__':
                                                                el.image.tags),
                                          client.containers.list()))
 
-    for container in interesting_containers:
-        nodes_gauge_list.append(
-            prometheus_client.Gauge(
-                ('Proposed_blocks_' + container.name).replace('-', '_'),
-                'Proposed blocks'))
+    WitnetMetrics(interesting_containers)
 
     print()
     print(interesting_containers)
@@ -60,4 +106,6 @@ if __name__ == '__main__':
     prometheus_client.start_http_server(8000)
     # Generate some requests.
     while True:
-        process_request(interesting_containers, nodes_gauge_list, get_proposed_blocks)
+        process_request(interesting_containers,
+                        WitnetMetrics.proposed_blocks_list,
+                        search_from_pattern)
