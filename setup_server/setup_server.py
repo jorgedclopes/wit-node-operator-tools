c342e572ff8cbb35ba5f62989789ef1a900b562c
Jorge Lopes <jorgedclopes@gmail.com>
2022-02-11 21:45:38 +0000

    Address sensitive informations and extra files

--- setup_server/setup_server.py
+++ setup_server/setup_server.py
@@ -3,20 +3,6 @@ import yaml
 
 
 def run():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname='45.130.104.48',
-                port=22,
-                username='admin',
-                password='!Wolfgang1791')
-
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('ls -a')
-    print(type(ssh_stdout))
-    print(ssh_stdout.read().decode('utf-8'))
-    return
-
-
-if __name__ == '__main__':
     file = open('setup_server/list_of_servers.yaml', 'r')
     content = yaml.safe_load(file)
 
@@ -34,4 +20,8 @@ if __name__ == '__main__':
             print(e)
             print(type(e))
             raise e
-    # run()
+    return
+
+
+if __name__ == '__main__':
+    run()
